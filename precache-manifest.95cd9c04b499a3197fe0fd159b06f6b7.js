2081a98c78ec18155b7302cf8e60b280e93d0b80
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2019-10-19 02:34:48 -0500

    Updates

---
+++ precache-manifest.95cd9c04b499a3197fe0fd159b06f6b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6625cbb7820e414aca36348086ea174",
+    "url": "/ToDo-List-App/index.html"
+  },
+  {
+    "revision": "1ab8afea5eeca3e1a981",
+    "url": "/ToDo-List-App/static/css/main.93395231.chunk.css"
+  },
+  {
+    "revision": "bae0a0481c3fd1587e52",
+    "url": "/ToDo-List-App/static/js/2.eea69693.chunk.js"
+  },
+  {
+    "revision": "1ab8afea5eeca3e1a981",
+    "url": "/ToDo-List-App/static/js/main.5c09f9ab.chunk.js"
+  },
+  {
+    "revision": "3f9be19b978ca3707120",
+    "url": "/ToDo-List-App/static/js/runtime-main.fb8e1af4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToDo-List-App/precache-manifest.5b69dcd8c4e7fefacb0806fc63826d24.js"
+  "/ToDo-List-App/precache-manifest.95cd9c04b499a3197fe0fd159b06f6b7.js"
 );
 
 self.addEventListener('message', (event) => {
